3c6ccfa6dd7988ef5ccee0ab7cde86c5ec029d0a
Craig Soutar <bodsda@ubuntu.com>
2023-10-13 12:40:54 +0100

    Tutorial 01 complete.

--- main.tf
+++ main.tf
@@ -1,1 +1,23 @@
-Hello, World
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "eu-west-1"
+}
+
+resource "aws_instance" "my-test-server" {
+  ami           = "ami-0b9fd8b55a6e3c9d5"
+  instance_type = "t1.micro"
+
+  tags = {
+    Name = "My Test Server"
+  }
+}
